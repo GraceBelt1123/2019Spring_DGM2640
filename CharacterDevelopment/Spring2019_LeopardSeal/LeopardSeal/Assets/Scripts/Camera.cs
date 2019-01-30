ea7624ab24b9766aa8cb32a113399b6066311e79
Unknown <undergo4570@gmail.com>
2019-01-30 12:03:46 -0700

    Committed

---
+++ CharacterDevelopment/Spring2019_LeopardSeal/LeopardSeal/Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+	public Transform target;
+	public float Speed = 0.125f;
+	public Vector3 offset;
+
+	void Start()
+	{
+		Vector3 focusPosition = target.position + offset;
+		Vector3 speedPosition = Vector3.Lerp(transform.position, focusPosition, Speed);
+		transform.position = speedPosition;
+	}
+}
---
+++ CharacterDevelopment/Spring2019_LeopardSeal/LeopardSeal/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+   public float Speed = 5;
+   public Rigidbody Player;
+   public bool iControl = true;
+   public float iSpeed = 10;
+
+   void Start()
+   {
+      Player = GetComponent<Rigidbody>();
+   }
+
+   void Update()
+   {
+      if (iControl == true)
+      {
+         float sugar = Input.GetAxis("Horizontal") * Speed;
+         Player.transform.Translate(sugar * Time.deltaTime, 0,0);
+      }
+   }
+
+   void Running()
+   {
+      if (iControl == true)
+      {
+         Input.GetKey(KeyCode.LeftShift);
+      }
+   }
+}
