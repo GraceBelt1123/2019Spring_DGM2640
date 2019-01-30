71999226562fe870ad6a5cd79ab4d56522e6c7fd
Unknown <undergo4570@gmail.com>
2019-01-29 21:23:28 -0700

    Committeed

--- CharacterDevelopment/Spring2019_LeopardSeal/LeopardSeal/Assets/Scripts/Movement.cs
+++ CharacterDevelopment/Spring2019_LeopardSeal/LeopardSeal/Assets/Scripts/Movement.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   public float Speed = 5;
+   public Rigidbody Player;
+   public bool iControl = true;
+   public float iSpeed = 10;
+
+   void Start()
+   {
+      Player = GetComponent<Rigidbody>();
+   }
+
+   void Update()
+   {
+      if (iControl == true)
+      {
+         float sugar = Input.GetAxis("Horizontal") * Speed;
+         Player.transform.Translate(sugar * Time.deltaTime, 0,0);
+      }
+   }
+
+   void Running()
+   {
+      if (iControl == true)
+      {
+         Input.GetKey(KeyCode.LeftShift);
+      }
+   }
 }
