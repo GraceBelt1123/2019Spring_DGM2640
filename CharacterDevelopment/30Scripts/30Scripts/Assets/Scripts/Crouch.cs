0c16cedef0c39775c7e14026f3343029505169d2
Unknown <undergo4570@gmail.com>
2019-01-30 21:45:19 -0700

    Committed

---
+++ CharacterDevelopment/30Scripts/30Scripts/Assets/Scripts/Crouch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+	/*public Rigidbody Player;
+	private CharacterController iCrouch;
+	
+	void Start()
+	{
+		Player = gameObject.GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.C))
+		{
+			Player.height = 1.0f;
+		}
+
+		else
+		{
+			Player.height = 1.8f;
+		}
+	}
+
+	void crouch()
+	{
+		Player
+	}*/
+}
---
+++ CharacterDevelopment/30Scripts/30Scripts/Assets/Scripts/Speed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+	/*public float iSpeed = 10;
+	public Rigidbody Player;
+	public bool Run = true;
+
+	void Start()
+	{
+		Player = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Run == true && Input.GetKey(KeyCode.LeftShift))
+		{
+			float sugar = iSpeed;
+			Player.transform.Translate(sugar * Time.deltaTime,0,0);
+		}	
+	}*/
+}
