86d662ff5e8afd19f4431e088957b3437197a7f5
Unknown <undergo4570@gmail.com>
2019-02-08 20:32:12 -0700

    Committeed

---
+++ CharacterDevelopment/30Scripts/30Scripts/Assets/Scripts/RedSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedSwitch : MonoBehaviour
+{
+	public GameObject Crush;
+
+	void Start()
+	{
+		Crush.GetComponent<Rigidbody>().useGravity = true;
+	}
+}
